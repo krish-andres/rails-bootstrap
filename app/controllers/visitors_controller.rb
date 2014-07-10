08400b2f88fe4c6930d7e0398b374a6ff1049b37
Krish <krish.andres@gmail.com>
2014-07-10 16:11:06 -0700

    add form to home page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,24 @@
 class VisitorsController < ApplicationController
 
 	def new
+		@visitor = Visitor.new
 	end 
+
+	def create
+		@visitor = Visitor.new(secure_params)
+		if @visitor.valid?
+			# TODO Save Data
+			redirect_to root_path, notice: "Chose #{@visitor.favorite}"
+		else 
+			render :new
+		end 
+	end 
+
+
+	private
+
+	def secure_params
+		params.require(:visitor).permit(:favorite, :comment)
+	end
 
 end
